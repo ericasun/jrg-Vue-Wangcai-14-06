006ee309784651c1bfd5a934c91966b9972519a5
ericasunchina <jagg1@126.com>
2024-05-20 17:03:15 +0800

    deploy

---
+++ precache-manifest.392c97ca83f665ece58d7fbc643eebdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30735fbc3211c13e193d",
+    "url": "/jrg-Vue-Wangcai-16-06/css/app.dbf93944.css"
+  },
+  {
+    "revision": "f3d4f61875b79882c3d877b20ea5b177",
+    "url": "/jrg-Vue-Wangcai-16-06/index.html"
+  },
+  {
+    "revision": "30735fbc3211c13e193d",
+    "url": "/jrg-Vue-Wangcai-16-06/js/app.6f8b7342.js"
+  },
+  {
+    "revision": "7e5bdce5e14b53d0f314",
+    "url": "/jrg-Vue-Wangcai-16-06/js/chunk-vendors.ae5b0e54.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/jrg-Vue-Wangcai-16-06/manifest.json"
+  },
+  {
+    "revision": "d3829550cf8facc6c7dc650bdaf6761d",
+    "url": "/jrg-Vue-Wangcai-16-06/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jrg-Vue-Wangcai-16-06/robots.txt"
+  }
+]);
